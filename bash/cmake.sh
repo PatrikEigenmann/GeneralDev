a31eae95778cd67b635402327e7ca4366d9cd9e4
PatrikEigenmann <p.eigenmann@gmx.net>
2024-10-25 20:44:17 -0700

    cmake.sh v00.02

--- bash/cmake.sh
+++ bash/cmake.sh
@@ -1,23 +1,21 @@
 #! /bin/sh
-# ----------------------------------------------------------------------------
-# cmake.sh - Compile all the .c and .cpp files with either the gcc or
-# the g++ in the active folder.
-# ----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------
+# cmake.sh - batch script to compile all *.c files in the active folder. The .c files will
+# compiled with the gcc compiler. If you have libraries to compile with it, just give them
+# as command line parameters to the script and the gcc compiler will take care of it.
+# -----------------------------------------------------------------------------------------
 # Author: Patrik Eigenmann
 # Contact: p.eigenmann@gmx.net
-# ----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------
 # Change Log:
-# Sat	2023-05-27	File created.				 Version 00.01
-# ----------------------------------------------------------------------------
+# Sat	2023-05-27	File created.				                                        Version 00.01
+# Fri 2024-10-25  Revised bash script.                                        Version 00.02
+# -----------------------------------------------------------------------------------------
 
-# Iterate through all the .c files and compile them with the gcc.
-# Supress the warnings if there aren't any .c in the folder.
-for c_files in *.c; do
-  echo "gcc $c_files -o ${c_files%.c}.o"
-  gcc -c $c_files -o ${c_files%.c}.o 2>/dev/null
-done
+# Check if any libraries are provided
+LIBS="$@"
 
-for cpp_files in *.cpp; do
-  echo "g++ $cpp_files -o ${cpp_files%.cpp}.o"
-  g++ -c $cpp_files -o ${cpp_files%.cpp}.o 2>/dev/null
+for file in *.c; do
+    echo "Compiling $file..."
+    gcc $file $LIBS -o "${file%.c}.out"
 done
