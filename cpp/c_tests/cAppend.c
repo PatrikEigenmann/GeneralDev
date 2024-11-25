b6110aceffcd38823a96822c21e4c9cac88f6c23
PatrikEigenmann <p.eigenmann@gmx.net>
2024-11-25 04:05:17 -0800

    cHangman v00.02

--- cpp/c_tests/cAppend.c
+++ cpp/c_tests/cAppend.c
@@ -32,12 +32,13 @@
 #include <stdarg.h>
 #include <string.h>
 
-// ---------------------------------------------------------------------------------------------------------------
+// ***************************************************************************************************************
 // The foo function demonstrates the dynamic enhancement of a foundational string by appending a user-provided
 // value. This process culminates in the display of the augmented string, showcasing the seamless integration of
 // flexible string manipulation within our robust framework.
+//
 // @param String test - User provided test string.
-// ---------------------------------------------------------------------------------------------------------------
+// ***************************************************************************************************************
 void foo(char *test) {
     char string[100] = "D:\\bin\\temp\\";
 
@@ -46,13 +47,14 @@ void foo(char *test) {
     printf("String: %s.man\n", string);
 }
 
-// ------------------------------------------------------------------------------------------------------------------
-// main - In the vast, uncharted realms of cyberspace, there exists a function. Not just any function, but the main
-// function. It is the gatekeeper, the sentinel that stands at the threshold of every C and C++ program. It is the
-// beginning and the end, the alpha and the omega.
+// ***************************************************************************************************************
+// main - In the vast, uncharted realms of cyberspace, there exists a function. Not just any function, but the
+// main function. It is the gatekeeper, the sentinel that stands at the threshold of every C and C++ program. It
+// is the beginning and the end, the alpha and the omega.
+//
 // @param int argc     - Command argument count
 // @param char **argv  - Command argument strings
-// ------------------------------------------------------------------------------------------------------------------
+// ***************************************************************************************************************
 int main() {
 
     foo("MyTest");
